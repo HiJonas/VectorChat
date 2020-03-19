e728ee70c09a2844db3496d9d5198f3c2b22aa8b
Angelika285 <angelika.sudheimer@web.de>
2020-03-19 22:30:33 +0100

    [Dev] changed ServerThread

--- src/Server/ServerThread.java
+++ src/Server/ServerThread.java
@@ -9,7 +9,6 @@ public class ServerThread extends Thread {
 
     public void run(){
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-        System.out.println("DU da?");
         try {
 		    if (in.readLine().startsWith("/log")) {
 			    System.out.println("naaa");
@@ -18,6 +17,5 @@ public class ServerThread extends Thread {
 		} catch (InterruptedException | IOException e) {
             //
         }
-        System.out.println("ciao");
     }
 }
